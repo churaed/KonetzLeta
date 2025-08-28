465057cde2ad1ba12a076fe5b0044eea7671b041
Alexey Churakov <churaed@gmail.com>
2025-08-28 16:31:42 +0800

    build(config): set base path for build output

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/KonetzLeta/',
   plugins: [react({
     babel: {
       plugins: ['styled-jsx/babel'],
